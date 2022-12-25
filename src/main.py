3f772e55d8938e38891a4de777abbb9d079e3e78
Mohammad Joshaghani <71117558+mohammadjoshaghani@users.noreply.github.com>
2022-12-25 19:58:42 +0330

    update

--- src/main.py
+++ src/main.py
@@ -5,20 +5,20 @@ from runner import Runner
 
 s_time = time.time()
 
-model   = "MLP-K-GP"#"MLP"
-distance= "HL"   # "KL"
+model   = "MLP"  #"MLP_K_GP"
+distance= "KL"   # "HL"
 epochs  = 100
 
-mode ='test' 
+mode ='train' 
 logger.info(f"\n### start {mode} phase for {model}_{distance}:\n")
 
 runner = Runner(mode, epochs, model, distance)
 runner.run()
 
-# mode='test'
-# runner._init(mode, epochs)
-# logger.info(f"\n### start {mode} phase for {model}_{distance}:\n")
-# runner.run()
+mode='test'
+runner._init(mode, epochs)
+logger.info(f"\n### start {mode} phase for {model}_{distance}:\n")
+runner.run()
 
 logger.info(f"\n total time: {time.time()-s_time :.2f} seconds.")
 logger.info("\n finish.")
