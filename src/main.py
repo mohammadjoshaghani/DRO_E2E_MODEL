1b4e1021c2d4f39fb65bc8c2261a8a2c86baed31
Mohammad Joshaghani <71117558+mohammadjoshaghani@users.noreply.github.com>
2023-01-10 14:23:56 +0330

    in test, valid we train model every 3 steps

--- src/main.py
+++ src/main.py
@@ -6,18 +6,19 @@ from runner import Runner
 s_time = time.time()
 
 # models ={"WaveCorr", "WaveCorr_Casual", "Equally_weighted"} 
-model   = "WaveCorr"  
+# model   = "WaveCorr"  
 distance= "HL"   # "KL"
 epochs  = 32
 weightDecay = 1e-3
-experiment_id = "002"
+experiment_id = "003"
 
-runner = Runner('train', 1, model, distance, weightDecay, experiment_id)
-for epoch in range(1, epochs+1):
-    for mode in ['train', 'valid', 'test']:
-        runner._init(mode, epoch)
-        logger.info(f"\n### start {mode} phase for {model}_{distance}:\n")
-        runner.run()
+for model in ["WaveCorr", "WaveCorr_Casual"]:
+    runner = Runner('train', 1, model, distance, weightDecay, experiment_id)
+    for epoch in range(1, epochs+1):
+        for mode in ['train', 'valid', 'test']:
+            runner._init(mode, epoch)
+            logger.info(f"\n### start {mode} phase for {model}_{distance}:\n")
+            runner.run()
 
 logger.info(f"\n total time: {time.time()-s_time :.2f} seconds.")
 logger.info("\n finish.")
