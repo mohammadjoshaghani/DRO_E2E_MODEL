e02a18aa2a5c43cb687d3deeccb6d8c6955078f3
Mohammad Joshaghani <71117558+mohammadjoshaghani@users.noreply.github.com>
2023-04-16 14:56:05 +0430

    add experiment.py

---
+++ src/experiment.py
@@ -0,0 +1,61 @@
+import time, os, logging
+from logger import logger
+from runner import Runner
+import wandb
+
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+class Optimizer:
+    def __init__(self, model, distance, ExpId):
+        self.model = model
+        self.distance = distance
+        self.ExpId = ExpId
+    
+    def objective(self):      
+        
+        wandb.init()
+        epochs, weightDecay, learning_rate = wandb.config.epochs, wandb.config.weightDecay, wandb.config.learning_rate
+        modes_loss = {}
+
+        # run model
+        self.runner = Runner('train', epochs, self.model, self.distance, weightDecay, self.ExpId)
+        for mode in ['train', 'valid', 'test']:
+            self.runner._init(mode, epochs)
+            logger.info(f"\n### start {mode} phase for {model}_{distance}_wd_{weightDecay}_epochs_{epochs}:\n")
+            self.runner.run()
+            # save loss for each mode
+            modes_loss[mode] = self.runner.portfolio_Final
+        # validation and test loss:
+        valid_loss, test_loss = modes_loss['valid'], modes_loss['test']
+        # Log model performance metrics to W&B
+        wandb.log({"valid_loss": valid_loss, "test_loss": test_loss})
+
+os.environ['WANDB_SILENT']="true"
+os.environ['WANDB_MODE']="offline"
+
+logger.info("\nstart:\n")
+s_time = time.time()
+
+# models ={"WaveCorr", "WaveCorr_Casual", "Equally_weighted"} 
+model   = "WaveCorr_Casual"  
+distance= ""   # "KL"
+ExpId = '01'
+
+optimizer = Optimizer(model, distance, ExpId)
+sweep_configs = {
+    "method": "random",
+    "metric": {"name": "valid_loss", "goal": "minimize"},
+    "parameters": {
+        "epochs": {"values": [1]},#[5, 10, 15, 20]
+        "weightDecay": {"values": [0.001, 0.003, 0.005, 0.009]},
+        "learning_rate": {"distribution": "uniform", "min": 0.0100, "max": 0.0400},
+        
+    },
+}
+
+sweep_id = wandb.sweep(sweep_configs, project=f"{model}_{distance}_{ExpId}")
+wandb.agent(sweep_id=sweep_id, function=optimizer.objective, count=2)
+
+logger.info(f"\n total time: {time.time()-s_time :.2f} seconds.")
+logger.info("\n finish.")
